a8027e77b0452c23d455c6d25069aecdf0f7fcf5
laj <larry.jones@reveal-energy.com>
2022-09-14 13:51:07 -0500

    Change App to manipulate the browser DOM
    To actually run this code, I had to add a button to `index.html`;
    however, `index.html` in the `dist` directory **does not** appear to be
    under source code control. I do not understand it, but I am ignoring it
    for now. (I'll undoubtedly regret this choice.)

--- fable-getting-started/src/App.fs
+++ fable-getting-started/src/App.fs
@@ -1,3 +1,13 @@
 module App
 
-printfn "Fable is up and running..."
+// Provides access from F# to the JavaScript Browse DOM API
+open Browser.Dom
+
+// Locate the "printMsg" button in the DOM
+let printMsgButton = document.getElementById "printMsg"
+
+// Change (mutate) the `onClick` handler to print a message to the debug console when I press the
+// `printMsg` button.
+printMsgButton.onclick <- fun eventArgs ->
+    printfn "Button clicked"
+    
