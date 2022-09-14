310630a551bd327a2887e64c2de3045c3b9b85bd
laj <larry.jones@reveal-energy.com>
2022-09-14 17:48:42 -0500

    Single increment / decrement counter

--- fable-getting-started/src/App.fs
+++ fable-getting-started/src/App.fs
@@ -3,11 +3,30 @@ module App
 // Provides access from F# to the JavaScript Browse DOM API
 open Browser.Dom
 
-// Locate the "printMsg" button in the DOM
-let printMsgButton = document.getElementById "printMsg"
+// Dom references for UI elements
+let incrementButton = document.getElementById("increment")
+let decrementButton = document.getElementById("decrement")
+let countView = document.getElementById("countView")
 
-// Change (mutate) the `onClick` handler to print a message to the debug console when I press the
-// `printMsg` button.
-printMsgButton.onclick <- fun eventArgs ->
-    printfn "Button clicked"
+// The model (state) of our application
+let mutable counter = 0
 
+let render = fun () ->
+    // Update the view based on the model
+    countView.innerText <- $"Count is at {counter}"
+    
+// Perform the initial render
+render ()
+
+// Attach event handlers to buttons
+incrementButton.onclick <- fun _eventArgs ->
+    // Update the model...
+    counter <- counter + 1
+    
+    render ()
+
+decrementButton.onclick <- fun _eventArgs ->
+    // Update the model 
+    counter <- counter - 1
+    
+    render ()
