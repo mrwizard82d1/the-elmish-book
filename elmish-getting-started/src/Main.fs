98d4b3691f8c14fdff550ac99f377cbd51598807
laj <larry.jones@reveal-energy.com>
2022-09-15 14:41:33 -0500

    Conditional rendering using if-then-else

--- elmish-getting-started/src/Main.fs
+++ elmish-getting-started/src/Main.fs
@@ -27,18 +27,28 @@ let update (msg: Msg) (state: State): State =
         
 // The view function (called `render` to communicate with developers familiar with React)
 let render (state: State) (dispatch: Msg -> unit) =
-    Html.div [
-        Html.button [
-            prop.onClick (fun _ -> dispatch Increment)
-            prop.text "Increment"
-        ]
+    // Illustrate conditional rendering using if-then-else
+    let headerText =
+        if state.Count % 2 = 0
+        then "Count is even"
+        else "Count is odd"
         
-        Html.button [
-            prop.onClick (fun _ -> dispatch Decrement)
-            prop.text "Decrement"
-        ]
+    let oddOrEvenMessage = Html.h1 headerText
         
-        Html.h1 state.Count
+    if state.Count < 0 then
+        // negative so do not render `oddOrEvenMessage`
+        Html.div [
+            Html.button [ prop.onClick (fun _ -> dispatch Increment); prop.text "+" ]
+            Html.div state.Count
+            Html.button [ prop.onClick (fun _ -> dispatch Decrement); prop.text "-" ]
+        ]
+    else
+        // non-negative so **render** `oddOrEvenMessage`
+        Html.div [
+            Html.button [ prop.onClick (fun _ -> dispatch Increment); prop.text "+" ]
+            Html.div state.Count
+            Html.button [ prop.onClick (fun _ -> dispatch Decrement); prop.text "-" ]
+            oddOrEvenMessage
     ]
     
 Program.mkSimple init update render
