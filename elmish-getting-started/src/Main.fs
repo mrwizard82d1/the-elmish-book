d438f3e800e0f90c3237dfa2290c7aa3cb59f769
laj <larry.jones@reveal-energy.com>
2022-09-17 13:22:45 -0500

    Change error handling to use Result<T, TError>
    Previous commits used Option<int> to handle errors but provided no
    additional information on the error.
    
    This commit uses a Result<int, string> type to presents a textual
    error message.

--- elmish-getting-started/src/Main.fs
+++ elmish-getting-started/src/Main.fs
@@ -7,42 +7,58 @@ open Feliz
 
 // A simple counter
 type State =
-    { NumberInput: int }
+    { NumberInput: int
+      ErrorMessage: string }
     
 // Messages that cause state to change
 type Msg =
     | SetNumberInput of int
+    | SetErrorMessage of string
     
 // Calculate the initial state of the application
 let init () =
-    { NumberInput = 0 }
+    { NumberInput = 0
+      ErrorMessage = "" }
     
 // Update the state based on messages received
 let update (msg: Msg) (state: State): State =
     match msg with
     | SetNumberInput toNumber ->
-        { state with NumberInput = toNumber }
+        { state with NumberInput = toNumber; ErrorMessage = "" }
+    | SetErrorMessage toMessage ->
+        { state with ErrorMessage = toMessage }
         
-let tryParseInt (toParse: string): Option<int> =
-    try Some (int toParse)
-    with | _ -> None
+let tryParseInt (toParse: string): Result<int, string> =
+    try Ok (int toParse)
+    with
+    | ex -> Error(ex.Message)
         
 // The view function (called `render` to communicate with developers familiar with React)
 let render (state: State) (dispatch: Msg -> unit) =
     Html.div [
-        Html.input [
-            prop.className "has-background-primary"
-            prop.valueOrDefault state.NumberInput
-            prop.onChange (fun (value: string) ->
-                           value
-                           |> tryParseInt
-                           |> Option.iter (SetNumberInput >> dispatch))
+        Html.div [
+            Html.input [
+                prop.className "has-background-primary"
+                prop.valueOrDefault state.NumberInput
+                prop.onChange (fun (value: string) ->
+                    match (tryParseInt value) with
+                    | Ok toNumber ->
+                        toNumber
+                        |> SetNumberInput
+                        |> dispatch
+                    | Error withMessage ->
+                        withMessage
+                        |> SetErrorMessage
+                        |> dispatch)
+            ]
+            Html.span [
+                prop.className "has-background-success"
+                prop.text (string state.NumberInput)
+            ]
         ]
-        Html.span [
-            prop.className "has-background-success"
-            prop.text (string state.NumberInput)
-        ]
+        Html.text state.ErrorMessage
     ]
+    
     
 Program.mkSimple init update render
 |> Program.withReactSynchronous "feliz-app"
