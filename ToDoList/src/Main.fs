e0f239b761c0e0c0b23ab917400050182f1327ab
laj <larry.jones@reveal-energy.com>
2022-10-04 13:46:55 -0500

    Clean up

--- ToDoList/src/Main.fs
+++ ToDoList/src/Main.fs
@@ -2,17 +2,7 @@ module Main
 
 open Elmish
 open Elmish.React
-open Feliz
 open ToDoList
-open Browser.Dom
-open Fable.Core.JsInterop
-
-importSideEffects "./styles/global.scss"
-
-ReactDOM.render(
-    ToDoList.appTitle,
-    document.getElementById "feliz-app"
-)
 
 Program.mkSimple ToDoList.init ToDoList.update ToDoList.toAdd
 |> Program.withReactSynchronous "feliz-app"
