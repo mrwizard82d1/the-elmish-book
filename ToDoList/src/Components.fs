8e28d6871baceef310eeae35de17dd07800f2a99
laj <larry.jones@reveal-energy.com>
2022-10-06 10:44:52 -0500

    Simplify `render` function by separating concerns

--- ToDoList/src/Components.fs
+++ ToDoList/src/Components.fs
@@ -29,55 +29,61 @@ module ToDoList =
            ToDoList = List.append state.ToDoList [ state.ToAdd ]
        }
        
-   let appTitle =
+   let renderAppTitle =
        Html.h1 [
            prop.className "title"
            prop.text "Elmish To Do List"
        ]
        
+   let renderToAdd (state: State) (dispatch: Msg -> unit) = 
+        Html.div [
+            prop.classes [ "field"; "has-addons" ]
+            prop.children [
+                Html.div [
+                    prop.classes [ "control"; "is-expanded" ]
+                    prop.children [
+                        Html.input [
+                            prop.classes [ "input"; "is-medium" ]
+                            prop.valueOrDefault state.ToAdd
+                            prop.onChange (SetToAdd >> dispatch)
+                        ]
+                    ]
+                ]
+                Html.div [
+                    prop.className "control"
+                    prop.children [
+                        Html.button [
+                            prop.classes [ "button"; "is-primary"; "is-medium" ]
+                            prop.onClick (fun _ -> dispatch ToAdd)
+                            prop.children [
+                                Html.i [prop.classes [ "fa"; "fa-plus" ]]
+                            ]
+                        ]
+                    ]
+                ]
+            ]
+        ]
+        
+   let renderToDoItems (state: State) (dispatch: Msg -> unit) =
+        Html.div [
+            prop.classes [ "content"; "is-medium" ]
+            prop.children [
+                Html.ul [
+                    prop.children (state.ToDoList
+                                   |> (List.map (fun t ->
+                                       Html.listItem [
+                                           prop.classes [ "is-primary" ]
+                                           prop.text t
+                                       ])))
+                ]
+            ]
+        ]
+       
    let render (state: State) (dispatch: Msg -> unit) =
        Html.div [
         prop.children [
-            appTitle
-            Html.div [
-                prop.classes [ "field"; "has-addons" ]
-                prop.children [
-                    Html.div [
-                        prop.classes [ "control"; "is-expanded" ]
-                        prop.children [
-                            Html.input [
-                                prop.classes [ "input"; "is-medium" ]
-                                prop.valueOrDefault state.ToAdd
-                                prop.onChange (SetToAdd >> dispatch)
-                            ]
-                        ]
-                    ]
-                    Html.div [
-                        prop.className "control"
-                        prop.children [
-                            Html.button [
-                                prop.classes [ "button"; "is-primary"; "is-medium" ]
-                                prop.onClick (fun _ -> dispatch ToAdd)
-                                prop.children [
-                                    Html.i [prop.classes [ "fa"; "fa-plus" ]]
-                                ]
-                            ]
-                        ]
-                    ]
-                ]
-            ]
-            Html.div [
-                prop.classes [ "content"; "is-medium" ]
-                prop.children [
-                    Html.ul [
-                        prop.children (state.ToDoList
-                                       |> (List.map (fun t ->
-                                           Html.listItem [
-                                               prop.classes [ "is-primary" ]
-                                               prop.text t
-                                           ])))
-                    ]
-                ]
-            ]
+            renderAppTitle
+            renderToAdd state dispatch
+            renderToDoItems state dispatch
         ]
        ]
